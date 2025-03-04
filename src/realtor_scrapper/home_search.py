f6bda9470431967031cba2dde9c1d95f65c56b9d
abarolli <obarolli@gmail.com>
2025-03-04 15:03:28 -0700

    Refactored web scrapping to ResultsPageIterator

--- src/realtor_scrapper/home_search.py
+++ src/realtor_scrapper/home_search.py
@@ -1,11 +1,14 @@
 
 from dataclasses import dataclass
 import json
+import math
+import re
 import requests
 from bs4 import BeautifulSoup as BS
 
 class RealtorSearchURLBuilder:
     __base_url: str = 'https://www.realtor.com/realestateandhomes-search'
+    __page_pattern = re.compile(r'/pg-(\d+)/?$')
 
     def __init__(self):
         self.__final_url: str = self.__base_url
@@ -47,7 +50,20 @@ class RealtorSearchURLBuilder:
         self.__validate_loc_added()
         self.__final_url += f'/show-{status}'
         return self
+    
+    def next_page(self):
+        match = self.__page_pattern.search(self.__final_url)
+        if match:
+            next_page = int(match.group(1)) + 1
+            self.__final_url = self.__page_pattern.sub(f'/pg-{next_page}', self.__final_url)
+        else:
+            self.__final_url += '/pg-2'
 
+    @property
+    def current_page(self):
+        match = self.__page_pattern.search(self.__final_url)
+        return int(match.group(1)) if match else 1
+            
     @property
     def url(self):
         return self.__final_url
@@ -57,50 +73,47 @@ class RealtorSearchURLBuilder:
 class RealtorProperty:
     price: int
     address: dict[str, str]
-    baths: float
-    beds: float
-    lot_sqft: int
-    sqft: int
+    baths: float = None
+    beds: float = None
+    lot_sqft: int = None
+    sqft: int = None
     sold_date: str = None
     sold_price: int = None
-        
-class RealtorProperties:
 
+
+class ResultsPageIterator:
     __headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36'
     }
 
-    def find(
-        self,
-        location: str,
-        price_range: tuple[int, int] = None,
-        property_types: tuple[str] = None,
-        beds: tuple[int, int] = None,
-        baths: tuple[int, int] = None,
-        listing_status: str = None
-    ) -> list[RealtorProperty]:
-        urlbuilder = RealtorSearchURLBuilder().location(location)
-        if price_range:
-            urlbuilder.price_range(*price_range)
-        if property_types:
-            urlbuilder.property_types(*property_types)
-        if beds:
-            urlbuilder.beds(beds[0], beds[1])
-        if baths:
-            urlbuilder.baths(baths[0], baths[1])
-        if listing_status:
-            urlbuilder.listing_status(listing_status)
+    def __init__(self, urlbuilder: RealtorSearchURLBuilder):
+        self.__urlbuilder = urlbuilder
+        self.__total_results_count = -1
+        self.__results = self.__fetch_homes()
 
-        return self.__get_homes(urlbuilder)
-        
-    def __get_homes(self, urlbuilder: RealtorSearchURLBuilder):
-        res = requests.get(urlbuilder.url, headers=self.__headers)
+    def has_next_page(self):
+        return self.__urlbuilder.current_page < self.__page_count()
+    
+    def for_each(self, f: callable):
+        for result in self.__results:
+            f(result)
+
+    def __page_count(self):
+        return math.ceil(self.__total_results_count / len(self.__results))
+
+    def next_page(self):
+        self.__urlbuilder.next_page()
+        self.__results = self.__fetch_homes()
+
+    def __fetch_homes(self):
+        res = requests.get(self.__urlbuilder.url, headers=self.__headers)
         bs = BS(res.text, "html.parser")
 
         datasrc = bs.find(id='__NEXT_DATA__')
         data = json.loads(datasrc.text)
-        properties = data.get('props').get('pageProps').get('properties')
 
+        properties = data.get('props').get('pageProps').get('properties')
+        self.__total_results_count = data.get('props').get('pageProps').get('totalProperties')
         results: list[RealtorProperty] = []
         for property in properties:
             description = property.get('description')
@@ -125,3 +138,29 @@ class RealtorProperties:
             )
 
         return results
+
+class RealtorProperties:
+
+    def find(
+        self,
+        location: str,
+        price_range: tuple[int, int] = None,
+        property_types: tuple[str] = None,
+        beds: tuple[int, int] = None,
+        baths: tuple[int, int] = None,
+        listing_status: str = None
+    ) -> ResultsPageIterator:
+        urlbuilder = RealtorSearchURLBuilder().location(location)
+        if price_range:
+            urlbuilder.price_range(*price_range)
+        if property_types:
+            urlbuilder.property_types(*property_types)
+        if beds:
+            urlbuilder.beds(beds[0], beds[1])
+        if baths:
+            urlbuilder.baths(baths[0], baths[1])
+        if listing_status:
+            urlbuilder.listing_status(listing_status)
+
+        return ResultsPageIterator(urlbuilder)
+        
